1b9d31f5e942cd622871b7dc0102726652d90ecf
Abir-07 <115392161+Abir-07@users.noreply.github.com>
2022-10-17 19:08:00 +0530

    Updated Quick sort in C++

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,72 +1,52 @@
-/* **************************************************************
- * TIME COMPLEXITY: O(N^2)
- * AVERAGE : O(NlogN)
- * BEST : O(NlogN)
- * **************************************************************/
-#include <iostream>
+#include
 using namespace std;
-const int N = 1e5+10;
-int arr[N];
 
-void swap(int *a, int *b)
-{
-    int temp = *a; // auxiliary space
-    *a = *b;
-    *b = temp;
+void swap(int arr[] , int pos1, int pos2){
+	int temp;
+	temp = arr[pos1];
+	arr[pos1] = arr[pos2];
+	arr[pos2] = temp;
 }
 
-void display(int *arr, int startIndex, int endIndex)
-{
-    for (int i = startIndex; i < endIndex; i++)
-        cout << arr[i] << " ";
-    cout << '\n';
+int partition(int arr[], int low, int high, int pivot){
+	int i = low;
+	int j = low;
+	while( i <= high){
+		if(arr[i] > pivot){
+			i++;
+		}
+		else{
+			swap(arr,i,j);
+			i++;
+			j++;
+		}
+	}
+	return j-1;
 }
 
-int partition(int *arr, int low, int high)
-{
-    int i = low + 1;  
-    int j = high;
-    int pivot = arr[low];
-
-    do {
-        while (arr[i] <= pivot) // for greater element 
-            i++;
-        while (arr[j] > pivot) // for smaller element
-            j--;
-        if (i < j) swap(&arr[i], &arr[j]);
-
-    } while (i < j);
-
-    swap(&arr[low], &arr[j]);
-    return j;
+void quickSort(int arr[], int low, int high){
+	if(low < high){
+	int pivot = arr[high];
+	int pos = partition(arr, low, high, pivot);
+	
+	quickSort(arr, low, pos-1);
+	quickSort(arr, pos+1, high);
+	}
 }
 
-void quickSort(int *arr, int low, int high)
+int main()
 {
-    int partitionIndex;
-
-    if (low < high)
-    {
-        partitionIndex = partition(arr, low, high);
-        quickSort(arr, low, partitionIndex - 1);
-        quickSort(arr, partitionIndex + 1, high);
-    }
-}
-
-int main(void)
-{
-    // int arr[] = {1, 8, 3, 9, 4, 5, 7};
-    int size;
-    cout << "Enter the size: "; cin >> size;
-    cout << "Enter the " << size << " elements" << endl ;
-    for (int i = 0; i < size; i++)
-        cin >> arr[i];
-    
-    cout << "\n Before Sort\n";
-    display(arr, 0, size);
-    cout << "\n QuickSort: ";
-    quickSort(arr, 0, size - 1);
-    display(arr, 0, size);
-
-    return 0;
+	int n ;
+	cout << " enter the size of array";
+	cin>>n;
+	int arr[n];
+	for( int i = 0 ; i < n; i++){
+		cin>> arr[i];
+	}
+	quickSort(arr, 0 , n-1);
+	cout<<"The sorted array is: ";
+	for( int i = 0 ; i < n; i++){
+		cout<< arr[i]<<"\t";
+	}
+	
 }
